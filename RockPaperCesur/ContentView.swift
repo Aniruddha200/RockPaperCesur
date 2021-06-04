2c855c38f2c1a21658031783ca7d3d211daec1cd
Aniruddha <aniruddha200baidya@protonmail.com>
2021-06-04 15:40:02 +0100

    Initial Commit

---
+++ RockPaperCesur/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperCesur
+//
+//  Created by administrator on 04/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RockPaperCesur/RockPaperCesurApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperCesurApp.swift
+//  RockPaperCesur
+//
+//  Created by administrator on 04/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperCesurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
